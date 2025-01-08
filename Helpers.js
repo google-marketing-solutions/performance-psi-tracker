7f06e821517f78adfddcd6a5e10243c19ead79c5
Adam Read <adamread@google.com>
2025-01-08 15:06:25 +0000

    Update Helpers comments and moved out test-related functions.

--- Helpers.js
+++ Helpers.js
@@ -1,6 +1,9 @@
 /**
- * Copyright 2023 Google LLC
+ * @copyright 2023 Google LLC
  *
+ * @fileoverview Helper functions used by the PSI Tracker
+ *
+ * @license
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,66 +16,52 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * This file is the core of the Performance PSI tracker project. This file must
- * be deployed to an appropriate Google Sheet to be used, along with the other
- * files in the project.
- */
-
 
 /**
- * A simple assertion function for testing.
- * Throws an error if the condition is not true, indicating the test failed.
+ * Show a confirmation modal indicating that we will call the Green Web Foundation's API.
+ * User can Proceed "YES" or Abort "NO" when asked if they want to proceed
  *
- * @param {boolean} condition The condition to test.
- * @param {string} message The error message to show if the test fails.
- */
-function assert(condition, message) {
-  if (!condition) {
-    throw new Error(message);
-  }
-}
-
-/**
- * Show a confirmation modal indicating that we will call a 3P. 
- * User can Proceed "YES" or Abord "NO" when asked if they want to proceed
+ * @return {boolean} Whether the call should be cancelled.
  */
 function showConfirmationModalCallGWF() {
-  var ui = SpreadsheetApp.getUi(); // Same variations.
+  var ui = SpreadsheetApp.getUi();
   var result = ui.alert(
     'Please confirm',
-    "Carbon's usage will be done through the use of services such as the Green Web Foundation’s Green Web Dataset. Those calls will send queries to The Green Web Foundation's dataset of green domains containing the information in this spreadsheet. Check https://developers.thegreenwebfoundation.org/api/greencheck/v3/check-single-domain/ for details. Do you still want to proceed?",
+    "Carbon's usage will be done through the use of services such as the Green Web Foundation’s "
+      + "Green Web Dataset. Those calls will send queries to The Green Web Foundation's dataset of "
+      + "green domains containing the information in this spreadsheet. Check "
+      + "https://developers.thegreenwebfoundation.org/api/greencheck/v3/check-single-domain/ for "
+      + "details. Do you still want to proceed?",
     ui.ButtonSet.YES_NO);
 
   // Process the user's response.
   if (result == ui.Button.YES) {
-    // User clicked "Yes".
-    // ui.alert('Confirmation received.'); 
     return false;
   } else {
-    // User clicked "No" or X in the title bar.
     ui.alert('Permission denied. Aborting');
     return true;
   }
 }
 
 /**
- * Show a confirmation modal indicating that we will call a 3P if the API is Green Domains
+ * Show a confirmation modal indicating that we will make a Green Domains API call.
+ *
+ * @returns {boolean} Whether the call should be cancelled.
  */
 function showConfirmationModalSustainability() {
   var ui = SpreadsheetApp.getUi(); // Same variations.
   var result = ui.alert(
     'Please confirm',
-    "Carbon's usage will be done through the use of services such as CO2.js. This action will run the different models in CO2.js taking into account the results of PSI Tracker. Please run Green Domains API first otherwise the model will assume that the domains are not green hosted. Do you want to continue?",
+    "Carbon's usage will be done through the use of services such as CO2.js. This action will run "
+      + "the different models in CO2.js taking into account the results of PSI Tracker. Please run "
+      + "Green Domains API first otherwise the model will assume that the domains are not green "
+      + "hosted. Do you want to continue?",
     ui.ButtonSet.YES_NO);
 
   // Process the user's response.
   if (result == ui.Button.YES) {
-    // User clicked "Yes".
-    // ui.alert('Confirmation received.'); 
     return false;
   } else {
-    // User clicked "No" or X in the title bar.
     ui.alert('Permission denied. Aborting');
     return true;
   }
@@ -81,8 +70,8 @@ function showConfirmationModalSustainability() {
 /**
  * Removes triggers by handler function.
  *
- * Given a function name, all triggers execute said function when fired will be
- * removed from the AppScript project.
+ * Given a function name, all triggers that execute said function when fired will be removed from
+ * the AppScript project.
  *
  * @param {string} functionName The name of the function run by the trigger.
  */
@@ -95,7 +84,7 @@ function deleteTriggers(functionName) {
 }
 
 /**
- * Remove Batch Triggers
+ * Remove all of the Batch Triggers that may be set by the solution.
  */
 function removeBatchTriggers() {
   deleteTriggers('runBatch');
@@ -106,30 +95,27 @@ function removeBatchTriggers() {
 }
 
 /**
- * Add a toast on the bottom right corner indicating status
+ * Add a toast on the bottom right corner with the given title and descriptive text.
+ *
+ * @param {string} title The title for the toast.
+ * @param {string} description The text to be used in the body of the toast.
  */
 function toast(title, description) {
   SpreadsheetApp.getActive().toast(description, title, -1);
 }
 
 /**
- * Shows a demo toast in the UI to demonstrate how the modal looks
- */
-function toastDemo() {
-  toast("Message", "Description")
-}
-
-
-/**
- * Dummy function to be able to click the Initialize button (UX feedback that having to double click to activate the features is suboptimal)
+ * Dummy function to be able to click the Initialize button.
+ *
+ * Just clicking the start item results in the user needing to authorize the solution and then click
+ * again to actually have it start. This allows authorization before clicking start.
  */
 function initialise() {
-  toast("Initialised", "Done")
+  toast("Initialized", "Done")
 }
 
 /**
  * Sets the trigger to run the tracker every day.
- *
  */
 function setDailyTrigger() {
   deleteTriggers('callPSIAPI');
@@ -149,7 +135,6 @@ function setDailyTrigger() {
 
 /**
  * Sets the trigger to run the CrUX API every day.
- *
  */
 function setCrUXDailyTrigger() {
   deleteTriggers('callCrUX');
@@ -175,68 +160,3 @@ function setDefaultValues() {
   SpreadsheetApp.flush();
 }
 
-/**
- * Returns a simplify version of a Lighthouse audit for test purposes
- */
-function getDummyData() {
-  return {
-    "id": "https://web.dev/",
-    "loadingExperience": {
-      "id": "https://web.dev/",
-      "metrics": {
-        "CUMULATIVE_LAYOUT_SHIFT_SCORE": {
-          "percentile": 8,
-        },
-        "EXPERIMENTAL_TIME_TO_FIRST_BYTE": {
-          "percentile": 2427
-        },
-        "FIRST_CONTENTFUL_PAINT_MS": {
-          "percentile": 4065
-        },
-        "FIRST_INPUT_DELAY_MS": {
-          "percentile": 150
-        },
-        "INTERACTION_TO_NEXT_PAINT": {
-          "percentile": 405
-        },
-        "LARGEST_CONTENTFUL_PAINT_MS": {
-          "percentile": 5025
-        }
-      },
-      "overall_category": "SLOW",
-      "initial_url": "https://web.dev/"
-    },
-    "originLoadingExperience": {
-      "id": "https://web.dev",
-      "metrics": {
-        "CUMULATIVE_LAYOUT_SHIFT_SCORE": {
-          "percentile": 8
-        },
-        "EXPERIMENTAL_TIME_TO_FIRST_BYTE": {
-          "percentile": 2406
-        },
-        "FIRST_CONTENTFUL_PAINT_MS": {
-          "percentile": 3985
-        },
-        "FIRST_INPUT_DELAY_MS": {
-          "percentile": 141
-        },
-        "INTERACTION_TO_NEXT_PAINT": {
-          "percentile": 411
-        },
-        "LARGEST_CONTENTFUL_PAINT_MS": {
-          "percentile": 4698
-        }
-      },
-      "overall_category": "SLOW",
-      "initial_url": "https://web.dev/"
-    },
-    "lighthouseResult": {
-      "requestedUrl": "https://web.dev/",
-      "finalUrl": "https://web.dev/",
-      "mainDocumentUrl": "https://web.dev/",
-      "finalDisplayedUrl": "https://web.dev/",
-      "lighthouseVersion": "11.0.0"
-    }
-  }
-}
